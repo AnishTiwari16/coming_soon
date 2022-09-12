dbbbded6e6eaae57a5416ebde07a3c83174a851e
Anish <anishtiwari12345@gmail.com>
2022-09-12 22:00:39 +0530

    upds

--- src/App.js
+++ src/App.js
@@ -3,45 +3,51 @@ import './App.css';
 import linkedinImg from '../src/linkedin.svg';
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <nav class="navbar navbar-expand-lg navbar-light bg-dark">
-                    <div className="container">
-                        <a class="navbar-brand" href="#">
-                            Navbar
-                        </a>
-                        <button
-                            class="navbar-toggler"
-                            type="button"
-                            data-toggle="collapse"
-                            data-target="#navbarSupportedContent"
-                            aria-controls="navbarSupportedContent"
-                            aria-expanded="false"
-                            aria-label="Toggle navigation"
-                        >
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
+        <section className="rr">
+            <div className="App ">
+                <header className="App-header">
+                    <nav class=" hg navbar navbar-expand-lg navbar-light bg-dark">
+                        <div className="container">
+                            <a class="navbar-brand" href="#">
+                                Navbar
+                            </a>
+                            <button
+                                class="navbar-toggler"
+                                type="button"
+                                data-toggle="collapse"
+                                data-target="#navbarSupportedContent"
+                                aria-controls="navbarSupportedContent"
+                                aria-expanded="false"
+                                aria-label="Toggle navigation"
+                            >
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
 
-                        <div
-                            class="collapse navbar-collapse"
-                            id="navbarSupportedContent"
-                        >
-                            <ul class="navbar-nav ml-auto">
-                                <li class="nav-item active">
-                                    <a class="nav-link" href="#">
-                                        <img src={linkedinImg} />{' '}
-                                        <span class="sr-only">(current)</span>
-                                    </a>
-                                </li>
-                            </ul>
+                            <div
+                                class="collapse navbar-collapse"
+                                id="navbarSupportedContent"
+                            >
+                                <ul class="navbar-nav ml-auto">
+                                    <li class="nav-item active">
+                                        <a class="nav-link" href="#">
+                                            <img src={linkedinImg} />{' '}
+                                            <span class="sr-only">
+                                                (current)
+                                            </span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
+                    </nav>
+                </header>
+                <section className="hchange">
+                    <div className="container">
+                        <h1>Coming Soon</h1>
                     </div>
-                </nav>
-            </header>
-            <section className="container">
-                <h1>Coming Soon</h1>
-            </section>
-        </div>
+                </section>
+            </div>
+        </section>
     );
 }
 
