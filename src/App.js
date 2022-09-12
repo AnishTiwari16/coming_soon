d9ed9d8e64d2116fa17bee3d4ed93fa585fb3e8d
Anish <anishtiwari12345@gmail.com>
2022-09-12 22:38:26 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -11,33 +11,15 @@ function App() {
                             <a class="navbar-brand" href="#">
                                 <h3 className="changeText">LiTt</h3>
                             </a>
-                            <button
-                                class="navbar-toggler"
-                                type="button"
-                                data-toggle="collapse"
-                                data-target="#navbarSupportedContent"
-                                aria-controls="navbarSupportedContent"
-                                aria-expanded="false"
-                                aria-label="Toggle navigation"
-                            >
-                                <span class="navbar-toggler-icon"></span>
-                            </button>
 
-                            <div
-                                class="collapse navbar-collapse"
-                                id="navbarSupportedContent"
-                            >
-                                <ul class="navbar-nav ml-auto">
-                                    <li class="nav-item active">
-                                        <a class="nav-link" href="#">
-                                            <img src={linkedinImg} />{' '}
-                                            <span class="sr-only">
-                                                (current)
-                                            </span>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
+                            <ul class="navbar-nav ml-auto">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#">
+                                        <img src={linkedinImg} />{' '}
+                                        <span class="sr-only">(current)</span>
+                                    </a>
+                                </li>
+                            </ul>
                         </div>
                     </nav>
                 </header>
